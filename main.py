e5fb99e84fcb3bdfc2a5323208f5a6455a6fa59f
Basil Egupov <basilegupov@gmail.com>
2023-12-03 20:06:28 +0200

    +Modul6

--- main.py
+++ main.py
@@ -1,75 +1,14 @@
-# 10. Напишіть функцію find_word, яка приймає два параметри: перший text та другий word. Функція виконує пошук 
-# зазначеного слова word у тексті text за допомогою функції search та повертає словник.
-# 
-# При виклику функції:
-# 
-# print(find_word(
-#     "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-#     "Python"))
-# Результат має бути наступного виду при збігу:
-# 
-# {
-#     'result': True,
-#     'first_index': 34,
-#     'last_index': 40,
-#     'search_string': 'Python',
-#     'string': 'Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.'
-# }
-# де
-# 
-# result — результат пошуку True або False
-# first_index — початкова позиція збігу
-# last_index — кінцева позиція збігу
-# search_string — частина рядка, в якому був збіг
-# string — рядок, переданий у функцію
-# Якщо збігів не виявлено:
-# 
-# print(find_word(
-#     "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-#     "python"))
-# Результат:
-# 
-# {
-#     'result': False,
-#     'first_index': None,
-#     'last_index': None,
-#     'search_string': 'python',
-#     'string': 'Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.'
-# }
-
-import re
+import sys
 
 
-def find_word(text, word):
-    res = {}
-    found =  re.search(word, text)
-    if found:
-        res['result']=True
-        res['first_index'] = found.start()
-        res['last_index'] = found.end()
-        res['search_string'] = found[0]
-        res['string'] = text
-    else:
-        res['result']=False
-        res['first_index'] = None
-        res['last_index'] = None
-        res['search_string'] = word
-        res['string'] = text
-        
-    return res
-        
-            
-            
-            
-print(find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-    "Python"))            
-
-print(find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-    "python")) 
-        
+def parse_args():
+    result =""
     
-        
+    if len(sys.argv)>1:
+        result += sys.argv[1]
+        for count in range(2,len(sys.argv)):
+            result+=' '+sys.argv[count]
             
-            
+    return result
+
+print(parse_args())
